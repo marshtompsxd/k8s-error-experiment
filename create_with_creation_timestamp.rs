c8cf5c300c066ebb26585fa3b8cb81fbc1cf5f10
marshtompsxd <xudongsunmarshtomp@gmail.com>
2023-05-31 22:51:24 -0500

    Add create_with_creation_timestamp.rs and create_with_status.rs

---
+++ create_with_creation_timestamp.rs
@@ -0,0 +1,41 @@
+// Nightly clippy (0.1.64) considers Drop a side effect, see https://github.com/rust-lang/rust-clippy/issues/9608
+#![allow(clippy::unnecessary_lazy_evaluations)]
+
+use anyhow::Result;
+use k8s_openapi::api::core::v1::Pod;
+use kube::{
+    api::{Api, PostParams},
+    Client,
+};
+use serde_json::json;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = Client::try_default().await?;
+    let pods: Api<Pod> = Api::default_namespaced(client);
+    let p: Pod = serde_json::from_value(json!({
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": {
+            "creationTimestamp": "2023-06-01T03:42:49Z",
+            "name": "blog"
+        },
+        "spec": {
+            "containers": [{
+              "name": "blog",
+              "image": "clux/blog:0.1.0"
+            }],
+        },
+    }))?;
+
+    let pp = PostParams::default();
+    // The creation succeeds and ignores the creationTimestamp field
+    match pods.create(&pp, &p).await {
+        Err(e) => {
+            println!("This creation fails with:\n{}", e);
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
